addaf83ee3fd0ab8335716ac1f0c7f7a302712f4
davir4189 <a2106897@institutmontilivi.cat>
2024-11-21 19:49:43 +0100

    sumar ,restar

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+# calculadora.py
+
+def sumar(a, b):
+    """
+    Retorna la suma de dos números.
+    """
+    return a + b
+
+def restar(a, b):
+    """
+    Retorna la resta de dos números.
+    """
+    return a - b
